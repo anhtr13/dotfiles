79b41f1196647867fecd0dc654a833dc40dc20c0
anhtr13 <anh.bigbro@gmail.com>
2025-10-07 02:53:52 +0700

    Minimize tmux configuration

--- git-update.sh
+++ git-update.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-count="$(git rev-list --count HEAD)"
+message="$1"
+if [[ -z "$message" ]]; then
+  message="Update #$(git rev-list --count HEAD)"
+fi
+
 git add -A
-git commit -m "Update #$count"
-git push origin main
+git commit -m "$message"
+
+rofi_theme="$XDG_CONFIG_HOME/rofi/themes/applet.rasi"
+yes='✓ Yes'
+no='✗ No'
+
+confirm_cmd() {
+  rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+    -theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
+    -theme-str 'listview {columns: 2; lines: 1;}' \
+    -theme-str 'element-text {horizontal-align: 0.5;}' \
+    -theme-str 'textbox {horizontal-align: 0.5;}' \
+    -dmenu \
+    -p 'Confirmation' \
+    -mesg 'Push commit?' \
+    -theme ${rofi_theme}
+}
+
+selected=$(echo -e "$no\n$yes" | confirm_cmd)
+if [[ "$selected" == "$yes" ]]; then
+  echo $message
+  git push origin main
+else
+  exit
+fi
